237474b718c6305a16b9c71dd6fd4f3ccba963fe
Yaty Lee <studi7o@mail.ru>
2017-12-19 15:54:21 +0800

    Generate Pelican site

---
+++ gnome.sh
@@ -0,0 +1,107 @@
+sudo apt install --no-install-recommends \
+ibus-libpinyin \
+gnome-boxes \
+gnome-builder \
+gnome-calculator \
+gnome-calendar \
+gnome-contacts \
+gnome-dictionary \
+gnome-documents \
+gnome-games-app \
+gnome-maps \
+gnome-music \
+gnome-photos \
+gnome-todo \
+gnome-weather \
+polari \
+totem \
+corebird \
+gnome-recipes \
+gnome-twitch \
+gnome-twitch-player-backend-gstreamer-cairo \
+gnome-twitch-player-backend-gstreamer-clutter \
+gnome-twitch-player-backend-gstreamer-opengl \
+pitivi \
+libclutter-1.0-dev \
+libclutter-1.0-doc \
+libclutter-gst-3.0-dev \
+libclutter-gst-3.0-doc \
+libclutter-gtk-1.0-dev \
+libclutter-gtk-1.0-doc \
+libgail-dev libgail-doc \
+libgail-3-dev libgail-3-doc \
+libgtk-3-dev libgtk-3-doc \
+libgtk2.0-dev libgtk2.0-doc \
+libgtkdatabox-dev libgtkdatabox-doc \
+libgtkdataboxmm-dev libgtkdataboxmm-doc \
+libgtkmm-2.4-dev libgtkmm-2.4-doc \
+libgtkmm-3.0-dev libgtkmm-3.0-doc \
+libgtksourceview-3.0-dev libgtksourceview-3.0-doc \
+libwebkit2gtk-4.0-dev libwebkit2gtk-4.0-doc \
+ffmpeg ffmpeg-doc \
+gstreamer1.0-plugins-bad gstreamer1.0-plugins-bad-doc \
+gstreamer1.0-plugins-ugly gstreamer1.0-plugins-ugly-doc \
+gstreamer1.0-fluendo-mp3 gstreamer1.0-libav \
+vim vim-doc vim-scripts vim-addon-manager exuberant-ctags \
+git git-doc repo mercurial subversion \
+p7zip-full p7zip-rar zip unzip rar unrar unrar-free unar \
+adb fastboot \
+minicom picocom openocd \
+mtd-utils u-boot-tools \
+polipo proxychains squid stunnel4 \
+pandoc \
+fping hping3 netcat nmap \
+samba smbclient samba-dsdb-modules samba-vfs-modules \
+vsftpd sqlite3 sqlite3-doc \
+qemu qemu-user-static \
+build-essential cppcheck glibc-doc gcc-doc libstdc++-7-doc make-doc \
+gcc-multilib g++-multilib gcc-arm-none-eabi gdb-arm-none-eabi \
+gdb gdb-doc \
+autoconf autoconf-doc automake libtool libtool-doc \
+cmake cmake-doc pkg-config \
+llvm clang clang-format llvm-4.0-doc clang-4.0-doc \
+golang \
+openjdk-8-jdk openjdk-8-doc openjdk-8-source openjdk-8-demo \
+libappindicator-dev libappindicator-doc \
+libatk1.0-dev libatk1.0-doc \
+libatkmm-1.6-dev libatkmm-1.6-doc \
+libbz2-dev bzip2-doc \
+libcairo2-dev libcairo2-doc \
+libcairomm-1.0-dev libcairomm-1.0-doc \
+libcanberra-dev libcanberra-doc \
+libcogl-dev libcogl-doc \
+libdee-dev libdee-doc \
+libdevil-dev \
+libflac-dev libflac-doc \
+libgda-5.0-dev libgda-5.0-doc \
+libgdamm5.0-dev libgdamm5.0-doc \
+libgdk-pixbuf2.0-dev libgdk-pixbuf2.0-doc \
+libglib2.0-dev libglib2.0-doc \
+libgstreamermm-1.0-dev libgstreamermm-1.0-doc \
+libice-dev libice-doc \
+libjson-glib-dev libjson-glib-doc \
+libltdl-dev libtool-doc \
+liblzma-dev liblzma-doc \
+libncurses5-dev ncurses-doc \
+libnotify-dev libnotify-doc \
+libpango1.0-dev libpango1.0-doc \
+libsdl-mixer1.2-dev \
+libsdl2-dev libsdl2-doc \
+libsigc++-2.0-dev libsigc++-2.0-doc \
+libsm-dev libsm-doc \
+libsoup2.4-dev libsoup2.4-doc \
+libssl-dev libssl-doc \
+libtelepathy-farstream-dev libtelepathy-farstream-doc \
+libtelepathy-glib-dev libtelepathy-glib-doc \
+libtelepathy-logger-dev libtelepathy-logger-doc \
+libtheora-dev libtheora-doc \
+libvdpau-dev libvdpau-doc \
+libx11-dev libx11-doc \
+libxml++2.6-dev libxml++2.6-doc \
+libxml2-dev libxml2-doc \
+llvm-dev \
+python-dev python-doc \
+python3-dev python3-doc \
+lib32ncurses5 lib32stdc++6 lib32z1 \
+shadowsocks \
+pelican python-bs4 python-typogrify ghp-import
